421c4df6ccb220aa46561914d9359b1625bf8b87
moctran <87555673+moctran@users.noreply.github.com>
2023-05-13 09:46:13 +0700

    Delete Week1 directory

--- Week1/HelloWorld2.java
+++
@@ -1,9 +0,0 @@
-package Week1;
-//Example 2: HelloWorld2.java
-//Text-printing program
-public class HelloWorld2 {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello \t world!");
-    } //end of method main
-}
